052b426e22b21ac6ac27503d1b96015dce2377bf
Lee Gyu Yeon <55003317+damie824@users.noreply.github.com>
2024-07-02 00:05:59 +0900

    ✍️ 로직 일부 수정 및 큐 기능 추가, 속도 측정 시간 축소 등

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,12 +1,10 @@
 use std::cmp::PartialEq;
 use std::path::Path;
 use std::sync::Arc;
-use std::{fs};
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Write;
-use base64::Engine;
 use base64::prelude::BASE64_STANDARD;
-use futures_util::StreamExt;
+use futures_util::{StreamExt, future::FutureExt};
 use queues::{IsQueue, Queue, queue};
 use reqwest::Client;
 use tokio::sync::Mutex;
@@ -14,8 +12,8 @@ use tokio::time::Instant;
 use json::{JsonValue, object};
 use tokio::spawn;
 
-#[derive(Debug, Clone)]
-enum DownloadStatus {
+#[derive(Debug, Clone, PartialEq)]
+pub enum DownloadStatus {
     Pending,
     Downloading,
     Extracting,
@@ -23,36 +21,24 @@ enum DownloadStatus {
     Error
 }
 
-/// Wrapper for downloader state and queue management
-#[doc(hidden)]
 pub struct DownloaderWrapper {
     pub status: DownloadStatus,
     pub queue: Queue<String>,
     pub current_speed: f64,
+    pub next_target: String,
     pub current_target: String,
     pub current_percent: f64
 }
 
 impl DownloaderWrapper {
-    /// Creates a new instance of DownloaderWrapper
-    fn new() -> DownloaderWrapper {
-        DownloaderWrapper {
+    fn new() -> Self {
+        Self {
             status: DownloadStatus::Pending,
             queue: queue![],
             current_speed: 0.0,
+            next_target: String::new(),
             current_target: String::new(),
             current_percent: 0.0
-        }
-    }
-}
-
-impl PartialEq for DownloadStatus {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (DownloadStatus::Pending, DownloadStatus::Pending) => true,
-            (DownloadStatus::Downloading, DownloadStatus::Downloading) => true,
-            (DownloadStatus::Paused, DownloadStatus::Paused) => true,
-            _ => false,
         }
     }
 }
@@ -60,108 +46,143 @@ impl PartialEq for DownloadStatus {
 pub struct DownloaderClient(pub Arc<Mutex<DownloaderWrapper>>);
 
 impl DownloaderClient {
-    /// Creates a new DownloaderClient and starts a background thread to manage downloads
-    pub fn new() -> DownloaderClient {
-        let client = DownloaderClient(
-            Arc::new(
-                Mutex::new(
-                    DownloaderWrapper::new()
-                )
-            )
-        );
-
-        client
+    pub fn new() -> Self {
+        Self(Arc::new(Mutex::new(DownloaderWrapper::new())))
     }
 
-    pub async fn register(&self,id:String, url: String, file_path: String) {
+    pub async fn register(&self, id: String, url: String, total_size: f64, file_path: String) {
+        let cloned_wrapper = Arc::downgrade(&self.0);
         let mut lock = self.0.lock().await;
         println!("url: {}, file_path: {}", url, file_path);
 
-        let target = Self::encode_target(id, url, file_path);
-        
+        let target = Self::encode_target(id, url, total_size, file_path);
+
         if lock.status == DownloadStatus::Pending {
-            self.start_download(target);
-        }
-        else {
+            spawn(async move {
+                if let Some(wrapper) = cloned_wrapper.upgrade() {
+                    let client = DownloaderClient(wrapper);
+                    client.start_download(target).await;
+                }
+            }.boxed());
+        } else {
             lock.queue.add(target).expect("Cannot add target to download queue.");
         }
     }
 
-    pub fn start_download(&self, target: String) {
+    pub async fn start_download(&self, target: String) {
         let cloned_wrapper = Arc::clone(&self.0);
         let target = Self::decode_target(target);
 
+        let url = target["url"].to_string();
+        let total_size = target["total_size"].as_f64().unwrap();
+        let file_path = target["file_path"].to_string();
+
+        let mut lock = self.0.lock().await;
+        lock.current_target = format!("{{\"url\":\"{}\",\"total_size\":{},\"file_path\":\"{}\"}}", url, total_size, file_path);
+
+        println!("current target : {}", lock.current_target);
+        print!("download starting..");
+
+        let cloned_wrapper_inner = Arc::downgrade(&cloned_wrapper);
         spawn(async move {
-            let client = DownloaderClient(cloned_wrapper);
-
-            println!("url: {}, path: {}",target["url"],target["file_path"] );
-
-            client.download(target["url"].to_string(),target["file_path"].to_string()).await.expect("Error");
+            if let Some(wrapper) = cloned_wrapper_inner.upgrade() {
+                let client = DownloaderClient(wrapper);
+                println!("url: {}, path: {}", url, file_path);
+                client.download(url, total_size, file_path).await.expect("Error");
+            }
         });
     }
 
-    /// Retrieves current download information
     pub async fn get_info(&self) -> String {
+        println!("decode started.");
         let lock = self.0.lock().await;
 
+        println!("target: {}", lock.current_target);
+
+        if lock.current_target.is_empty() {
+            return "No current target".to_string();
+        }
+
+        let decoded = match json::parse(&lock.current_target) {
+            Ok(value) => value,
+            Err(e) => {
+                println!("Error parsing JSON: {}", e);
+                return "Error parsing current target".to_string();
+            }
+        };
+
+        println!("decoded: {}", json::stringify(decoded.clone()));
+
         let info_object = object! {
-            id: lock.current_target.to_string(),
+            id: decoded["id"].to_string(),
             speed: lock.current_speed.to_string(),
             percent: lock.current_percent.to_string()
         };
-
         json::stringify(info_object)
     }
 
     pub async fn pause_download(&self) {
         let mut lock = self.0.lock().await;
-
         lock.status = DownloadStatus::Paused;
     }
 
     pub async fn resume_download(&self) {
         let lock = self.0.lock().await;
-        let target = Self::decode_target(lock.current_target.to_string());
+        let target = Self::decode_target(lock.next_target.to_string());
         let cloned_wrapper = Arc::clone(&self.0);
 
         spawn(async move {
             let client = DownloaderClient(cloned_wrapper);
-
-            println!("url: {}, path: {}",target["url"],target["file_path"] );
-
-            client.download(target["url"].to_string(),target["file_path"].to_string()).await.expect("Error");
+            println!("url: {}, path: {}", target["url"], target["file_path"]);
+            client.download(target["url"].to_string(), target["total_size"].as_f64().unwrap(), target["file_path"].to_string()).await.expect("Error");
         });
     }
 
-    fn decode_target(target: String)-> JsonValue {
-        let decoded_bytes = BASE64_STANDARD.decode(target).unwrap();
-        let decoded_str = String::from_utf8(decoded_bytes).unwrap();
-        println!("decoded : {}",decoded_str);
-        json::parse(&decoded_str).unwrap()
+    pub fn decode_target(target: String) -> JsonValue {
+        match base64::Engine::decode(&BASE64_STANDARD, &target) {
+            Ok(decoded_bytes) => match String::from_utf8(decoded_bytes) {
+                Ok(decoded_str) => {
+                    println!("decoded : {}", decoded_str);
+                    match json::parse(&decoded_str) {
+                        Ok(parsed_json) => parsed_json,
+                        Err(e) => {
+                            println!("Error parsing JSON: {}", e);
+                            JsonValue::Null
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Error converting bytes to string: {}", e);
+                    JsonValue::Null
+                }
+            },
+            Err(e) => {
+                println!("Error decoding base64: {}", e);
+                JsonValue::Null
+            }
+        }
     }
 
-    fn encode_target(id: String, url:String, file_path:String)-> String {
+    fn encode_target(id: String, url: String, total_size: f64, file_path: String) -> String {
         let stringified_target = json::stringify(object! {
             id: id,
             url: url,
+            total_size: total_size,
             file_path: file_path
         });
-        println!("stringified : {}",stringified_target);
-        BASE64_STANDARD.encode(stringified_target)
+        println!("stringified : {}", stringified_target);
+        base64::Engine::encode(&BASE64_STANDARD, stringified_target)
     }
 
-    /// Handles the actual file download
-    async fn download(&self, url: String, file_path: String) -> Result<(), String> {
-        println!("Starting download");
+    async fn download(&self, url: String, total_size: f64, file_path: String) -> Result<(), String> {
+        println!("Starting download to {}/,progress", file_path);
 
         let client = Client::new();
 
         let mut lock = self.0.lock().await;
 
-        let progress_path = format!("{}.progress", file_path);
+        let progress_path = format!("{}/.progress", file_path);
         let mut start_byte = 0;
-
-        let total_size = 5000;
 
         println!("got client total size");
 
@@ -184,72 +205,83 @@ impl DownloaderClient {
         println!("File created. Overwriting..");
 
         let mut stream = response.bytes_stream();
-        let mut last_time = Instant::now();
+        let last_time = Instant::now();
         let mut downloaded_bytes = 0u64;
-
+        
         while let Some(item) = stream.next().await {
             if lock.status == DownloadStatus::Paused {
                 break;
             }
-
             let chunk = item.map_err(|e| e.to_string())?;
             file.write_all(&chunk).map_err(|e| e.to_string())?;
             start_byte += chunk.len() as u64;
             downloaded_bytes += chunk.len() as u64;
 
             let elapsed_time = last_time.elapsed().as_secs_f64();
-
-            if elapsed_time > 1.0 {
-                let speed = downloaded_bytes as f64 / elapsed_time;
-                let percent = (start_byte as f64 / total_size as f64) * 100.0;
-                println!("Download speed: {:.2} KB/s, Progress: {:.2}%", speed / 1024.0, percent);
-
+            if elapsed_time > 0.1 {
+                let speed = downloaded_bytes as f64 / elapsed_time * 10.0;
+                let percent: f64 = (downloaded_bytes as f64 / total_size) * 100.0;
+                println!("Download speed: {:.2} KB/s, Progress: {:.0}%", speed / 1024.0, percent);
+                println!("Downloaded: {}b, Total: {}b", downloaded_bytes, total_size);
                 lock.current_percent = percent;
                 lock.current_speed = (speed / 1024.0).floor();
-
-                downloaded_bytes = 0;
-                last_time = Instant::now();
             }
-
-            downloaded_bytes = 0;
-            last_time = Instant::now();
-
-            fs::write(&progress_path, start_byte.to_string()).map_err(|e| e.to_string())?;
         }
 
-        fs::remove_file(&progress_path).map_err(|e| e.to_string())?;
-
-
-        let cloned_wrapper = Arc::clone(&self.0);
+        let cloned_wrapper = Arc::downgrade(&self.0);
 
         spawn(async move {
-            let client = DownloaderClient(cloned_wrapper);
-            client.extract(Path::new(&file_path)).await;
+            if let Some(wrapper) = cloned_wrapper.upgrade() {
+                let client = DownloaderClient(wrapper);
+                client.extract(Path::new(&file_path)).await;
+            }
         });
 
         Ok(())
+    
     }
 
-    /// Extracts a downloaded file
     async fn extract(&self, path: &Path) {
         let mut lock = self.0.lock().await;
-
         lock.status = DownloadStatus::Extracting;
 
-        println!("current path : {}/.progress",path.to_string_lossy());
+        println!("extracting.. : {}/.progress", path.to_string_lossy());
 
-        fs::rename(format!("{}/.progress",path.to_string_lossy()), format!("{}/downloaded.zip",path.to_string_lossy())).expect("error occurred during renamed file.");
+        fs::rename(format!("{}/.progress", path.to_string_lossy()), format!("{}/downloaded.zip", path.to_string_lossy())).expect("error occurred during renamed file.");
 
-        let file = File::open(format!("{}/downloaded.zip",path.to_string_lossy())).expect("Error opening file");
+        let file = File::open(format!("{}/downloaded.zip", path.to_string_lossy())).expect("Error opening file");
         if let Err(e) = zip_extract::extract(file, path, true) {
             println!("Error during extraction: {}", e);
             lock.status = DownloadStatus::Error;
             return;
         }
+
         self.delete(path).await;
+        lock.current_target = String::new();
+        lock.status = DownloadStatus::Pending;
     }
 
     async fn delete(&self, path: &Path) {
-        fs::remove_file(format!("{}/downloaded.zip",path.to_string_lossy())).expect("error occurred during renamed file.");
+        println!("deleting {}/downloaded.zip", path.to_string_lossy());
+        let remove_result = fs::remove_file(format!("{}/downloaded.zip", path.to_string_lossy()));
+        match remove_result {
+            Ok(_) => println!("File deleted successfully."),
+            Err(e) => println!("Failed to delete file: {}", e),
+        }
+    }
+
+    pub async fn next(&self) {
+        println!("calling next download..");
+        let mut lock = self.0.lock().await;
+        if let Ok(next_target) = lock.queue.remove() {
+            lock.next_target = next_target.clone();
+            let cloned_self = Arc::clone(&self.0);
+            tokio::spawn(async move {
+                let client = DownloaderClient(cloned_self);
+                client.start_download(next_target).await;
+            });
+        } else {
+            lock.status = DownloadStatus::Pending;
+        }
     }
 }
--- tests/test-main.rs
+++ tests/test-main.rs
@@ -1,5 +1,4 @@
 use std::time::Duration;
-use futures_util::StreamExt;
 use declartive_download::DownloaderClient;
 
 #[tokio::test]
@@ -7,22 +6,18 @@ async fn test() -> Result<(), Box<dyn std::error::Error>> {
     let client = DownloaderClient::new();
 
     println!("Registering new download..");
-    client.register("1".to_string(), "http://localhost:8080/games/download/odin-1711812665673".to_string(),"D:/Programming/Odin/test".to_string()).await;
-
-    tokio::time::sleep(Duration::new(0, 15)).await;
-
-    println!("Getting info..");
-    println!("Got status: {}",client.get_info().await);
-
-    tokio::time::sleep(Duration::new(10, 0)).await;
-
-    println!("Getting info..");
-    println!("Got status: {}",client.get_info().await);
-
-    tokio::time::sleep(Duration::new(10, 0)).await;
-
-    println!("Getting info..");
-    println!("Got status: {}",client.get_info().await);
+    client.register("1".to_string(), "http://localhost:8080/games/download/odin-1711812665673".to_string(), 2928778.0, "D:/Programming/Odin/test".to_string()).await;
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    client.register("1".to_string(), "http://localhost:8080/games/download/odin-1719408830366".to_string(), 57886032.0, "D:/Programming/Odin/test".to_string()).await;
+    for _ in 0..10 {
+        println!("Getting info..");
+        let info = client.get_info().await;
+        if info == "No current target" {
+            client.next().await;
+        }
+        println!("Got status: {}", info);
+        tokio::time::sleep(Duration::from_secs(3)).await;
+    }
 
     Ok(())
 }
